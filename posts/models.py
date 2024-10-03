ddf718004c77add55db2de5d4b5200e8a61b8770
osm3rr <osm3rr@adakademy.com>
2024-10-03 18:13:28 -0400

    model added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text[:50]
+
+
